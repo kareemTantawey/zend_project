74f492449b9e76abf80476de33fe838f028a26b1
fathitarek <ftarek@fcih1.com>
2016-04-22 18:53:03 +0200

    layout2

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,11 +6,19 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+          
+       
     }
+    
 
     public function indexAction()
     {
         // action body
+         $category_model = new Application_Model_Categories();
+        $this->view->categories = $category_model->listCategory();
+        // call forums
+        $forum_model = new Application_Model_Forums();
+        $this->view->forums = $forum_model->listForum();
     }
 
 
